f43052a0f6efd5b3758fa1fc5690788dce5e9b4f
Nik Stires <nik.stires@gmail.com>
2020-08-11 11:39:23 -0700

    fixed image sizes and nav

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ const navSlide = () => {
         burger.classList.toggle('toggle');
     });
 
+    nav.addEventListener('click', () => {
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s';
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
 }
 
 navSlide();
